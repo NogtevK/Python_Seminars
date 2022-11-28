9ca96e395332a3c592d73eecd52049784f14da11
Kirill Nogtev <nogtev.kirill@yandex.ru>
2022-11-28 22:08:22 +0400

    hw1

--- GB/Homework_PY_gb1.py
+++ GB/Homework_PY_gb1.py
@@ -49,6 +49,22 @@ elif x_number == 0:
 elif y_number == 0:
     print('точка находится на оси Y')
 
+# 4-Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y).
+
+# Пример:
+# - quarter = 1 = x∈(0, ∞) u y∈(0,∞)
+
+number_of_quarter = int(input('Введите номер четверти:\n'))
+if number_of_quarter not in range(1,5):
+    print('Вы ввели неверное число')
+elif number_of_quarter == 1:
+    print('x∈(0, ∞) u y∈(0,∞)')
+elif number_of_quarter == 2:
+    print('x∈(0,-∞) u y∈(0,∞)')
+elif number_of_quarter == 3:
+    print('x∈(0,-∞) u y∈(0,-∞)')
+else:
+    print('x∈(0,∞) u y∈(0,-∞)')
 
 
 
